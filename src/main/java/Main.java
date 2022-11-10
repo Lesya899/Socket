939bc1364097ca1ff6060b697429f2b0fa11ff46
Lesya899 <aleks_ni91@mail.ru>
2022-11-11 00:08:30 +0300

    socket for messaging

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,14 @@ import java.net.Socket;
 
 
 public class Main {
+    public static int PORT = 5050;
     public static void main(String[] args) {
-        try {
-            ServerSocket serverSocket = new ServerSocket(5050, 10);
-            System.out.println("Server started");
+        try(ServerSocket serverSocket = new ServerSocket(PORT)){
+            java.util.logging.Logger.getGlobal().info("Server started");
             while (true) {
                 Socket socket = serverSocket.accept(); // ожидаем подключения к этому сокету и принимает его
-                new ServerMessage(socket);
+                new ServerMessage(socket).start();
+
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
--- src/main/java/ServerMessage.java
+++ src/main/java/ServerMessage.java
@@ -4,24 +4,24 @@ import java.net.Socket;
 public class ServerMessage extends Thread{
     private Socket socket;
     private BufferedReader in; // поток чтения из сокета
-    private BufferedWriter out; // поток записи в сокет
+    private PrintWriter out; // поток записи в сокет
 
     public ServerMessage(Socket socket) throws IOException {
         this.socket = socket;
-        in = new BufferedReader(new InputStreamReader(socket.getInputStream())); //принимаем сообщения
-        out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())); //отправляем сообщения
-        start(); // вызываем метод run()
     }
 
+
+    @Override
     public void run() {
-        try {
+        try{
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream())); //принимаем сообщения
+        out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true); //отправляем сообщения
             while (true) {
                 String mes = in.readLine(); //ждём пока клиент напишет
                 if (mes.equals("Bye."))
                     break;
-                    out.write(mes + "\n"); //отвечаем клиенту
-                    out.flush(); //выталкиваем все из буфера
-                }
+                    out.println(mes);
+            }
         }catch (IOException exp) {
             System.out.println(exp.getMessage());
         }finally {
